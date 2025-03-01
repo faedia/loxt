902a95ffdb81505976c22365d0dffb1f6b125416
Emma Tomlinson <t97emma@gmail.com>
2025-03-01 17:42:14 +0000

    Update to use a sequence of StaticVector to avoid large reallocations

--- include/loxt/lexer.hpp
+++ include/loxt/lexer.hpp
@@ -1,12 +1,90 @@
 #pragma once
 
-#include <deque>
+#include <stdint.h>
+
 #include <string>
 #include <string_view>
 #include <unordered_map>
 #include <vector>
 
 namespace loxt {
+
+constexpr std::size_t Default_Block_Size = (1 << 15);
+
+template <class T, std::size_t blockSize = Default_Block_Size>
+class StaticVector {
+  template <class S>
+  using Block = std::vector<S>;
+
+ public:
+  class Iterator {
+   public:
+    Iterator() = delete;
+
+    auto operator++() -> Iterator& {
+      element++;
+      if (element == block->size()) {
+        block++;
+        element = 0;
+      }
+      return *this;
+    }
+
+    auto operator*() -> T& { return *(block->begin() + element); }
+
+    auto operator->() -> T* { return (block->begin() + element).operator->(); }
+
+    auto operator<=>(const Iterator& rhs) const = default;
+
+   private:
+    Iterator(const typename std::vector<Block<T>>::iterator& blk,
+             std::size_t elem)
+        : block(blk), element(elem) {}
+
+    friend class StaticVector<T>;
+
+    typename std::vector<Block<T>>::iterator block;
+    std::size_t element = 0;
+  };
+
+  StaticVector() { new_block(); }
+
+  auto begin() -> Iterator { return {m_Blocks.begin(), 0}; }
+
+  auto end() -> Iterator { return {m_Blocks.end(), 0}; }
+
+  auto operator[](std::size_t idx) -> T& {
+    return m_Blocks[idx / blockSize][idx % blockSize];
+  }
+
+  auto push_back(const T& element) {
+    ++m_Size;
+    m_Blocks.back().push_back(element);
+    if (m_Blocks.back().size() == blockSize) {
+      new_block();
+    }
+  }
+
+  template <class... Args>
+  auto emplace_back(Args&&... args) -> void {
+    ++m_Size;
+    m_Blocks.back().emplace_back(std::forward<Args>(args)...);
+    if (m_Blocks.back().size() == blockSize) {
+      new_block();
+    }
+  }
+
+  [[nodiscard]] auto size() const -> std::size_t { return m_Size; }
+
+ private:
+  void new_block() {
+    m_Blocks.emplace_back();
+    m_Blocks.back().reserve(blockSize);
+  }
+
+  std::vector<Block<T>> m_Blocks;
+  std::size_t m_Size = 0;
+};
 
 class TokenKind {
   enum class Kind : uint8_t {
@@ -40,21 +118,8 @@ struct SourceLocation {
   int column;
   std::string::const_iterator pos;
 
-  auto operator++() -> SourceLocation& {
-    if (*pos++ == '\n') {
-      column = 1;
-      ++line;
-    } else {
-      ++column;
-    }
-    return *this;
-  }
-
-  auto operator++(int) -> SourceLocation {
-    auto old = *this;
-    this->operator++();
-    return old;
-  }
+  auto operator++() -> SourceLocation&;
+  auto operator++(int) -> SourceLocation;
 };
 
 using Identifier = unsigned int;
@@ -74,13 +139,13 @@ struct Token {
 
 class TokenList {
  public:
-  using Iterator = std::vector<Token>::iterator;
-  using ConstIterator = std::vector<Token>::const_iterator;
+  using Iterator = StaticVector<Token>::Iterator;
 
   [[nodiscard]] auto begin() -> Iterator { return m_Tokens.begin(); }
-  [[nodiscard]] auto begin() const -> ConstIterator { return m_Tokens.begin(); }
+  // [[nodiscard]] auto begin() const -> ConstIterator { return
+  // m_Tokens.begin(); }
   [[nodiscard]] auto end() -> Iterator { return m_Tokens.end(); }
-  [[nodiscard]] auto end() const -> ConstIterator { return m_Tokens.end(); }
+  // [[nodiscard]] auto end() const -> ConstIterator { return m_Tokens.end(); }
 
   [[nodiscard]] auto size() const -> std::size_t { return m_Tokens.size(); }
 
@@ -107,7 +172,7 @@ class TokenList {
  private:
   explicit TokenList(const std::string& source) : m_Source(source) {}
 
-  std::vector<Token> m_Tokens;
+  StaticVector<Token> m_Tokens;
   std::unordered_map<std::string_view, Identifier> m_IdentifierMap;
   std::vector<std::string_view> m_Identifiers;
   std::vector<std::string> m_StringLiteral;
